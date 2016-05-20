53702303a00c1e4078d03502407a6c3916a29d68
Zhou Xinan <zhouxinan@protonmail.ch>
2016-05-20 22:35:26 +0800

    Add Access.WRITE_ONLY for userPassword.

--- ExcitedMap/src/main/java/com/excitedmap/pojo/User.java
+++ ExcitedMap/src/main/java/com/excitedmap/pojo/User.java
@@ -1,53 +1,57 @@
 package com.excitedmap.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 public class User {
-    private Integer userId;
+	private Integer userId;
 
-    private String userName;
+	private String userName;
 
-    private String userPassword;
+	@JsonProperty(access = Access.WRITE_ONLY)
+	private String userPassword;
 
-    private String userAvatarPath;
+	private String userAvatarPath;
 
-    private String userEmail;
+	private String userEmail;
 
-    public Integer getUserId() {
-        return userId;
-    }
+	public Integer getUserId() {
+		return userId;
+	}
 
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
 
-    public String getUserName() {
-        return userName;
-    }
+	public String getUserName() {
+		return userName;
+	}
 
-    public void setUserName(String userName) {
-        this.userName = userName == null ? null : userName.trim();
-    }
+	public void setUserName(String userName) {
+		this.userName = userName == null ? null : userName.trim();
+	}
 
-    public String getUserPassword() {
-        return userPassword;
-    }
+	public String getUserPassword() {
+		return userPassword;
+	}
 
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword == null ? null : userPassword.trim();
-    }
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword == null ? null : userPassword.trim();
+	}
 
-    public String getUserAvatarPath() {
-        return userAvatarPath;
-    }
+	public String getUserAvatarPath() {
+		return userAvatarPath;
+	}
 
-    public void setUserAvatarPath(String userAvatarPath) {
-        this.userAvatarPath = userAvatarPath == null ? null : userAvatarPath.trim();
-    }
+	public void setUserAvatarPath(String userAvatarPath) {
+		this.userAvatarPath = userAvatarPath == null ? null : userAvatarPath.trim();
+	}
 
-    public String getUserEmail() {
-        return userEmail;
-    }
+	public String getUserEmail() {
+		return userEmail;
+	}
 
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail == null ? null : userEmail.trim();
-    }
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail == null ? null : userEmail.trim();
+	}
 }
