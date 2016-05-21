7c5485923def4c8a6b0c172ff3c21913d0cc0987
Tianlang <133020100024@fudan.edu.cn>
2016-05-22 01:43:22 +0800

    Merge remote-tracking branch 'refs/remotes/origin/master'
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    
    增加spot

--- ExcitedMap/src/main/java/com/excitedmap/dao/SpotMapperImpl.java
+++ ExcitedMap/src/main/java/com/excitedmap/dao/SpotMapperImpl.java
@@ -6,4 +6,8 @@ import com.excitedmap.pojo.Spot;
 
 public interface SpotMapperImpl extends SpotMapper {
 	List<Spot> selectBySpotCategoryId(int spotCategoryId);
+
+	List<Spot> selectByOrderByFavoriteCountWithLimit(int limit);
+	
+	List<Spot> selectByOrderByWishCountWithLimit(int limit);
 }
