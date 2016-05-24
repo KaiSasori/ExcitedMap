d4efd7f36fbf69ba4a1d26d583980d78c6c38811
Zhou Xinan <zhouxinan@protonmail.ch>
2016-05-24 16:44:33 +0800

    Add select by spot_category_id constraint.

--- ExcitedMap/src/main/java/com/excitedmap/dao/SpotMapperImpl.java
+++ ExcitedMap/src/main/java/com/excitedmap/dao/SpotMapperImpl.java
@@ -2,18 +2,24 @@ package com.excitedmap.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.excitedmap.pojo.Spot;
 
 public interface SpotMapperImpl extends SpotMapper {
 	List<Spot> selectBySpotCategoryId(int spotCategoryId);
 
-	List<Spot> selectByOrderByFavoriteCountWithLimit(int limit);
-	
-	List<Spot> selectByOrderByWishCountWithLimit(int limit);
-	
-	List<Spot> selectByOrderByFootprintCountWithLimit(int limit);
-	
-	List<Spot> selectByOrderByAverageReviewRating(int limit);
-	
+	List<Spot> selectByOrderByFavoriteCountWithLimit(@Param("spotCategoryId") int spotCategoryId,
+			@Param("limit") int limit);
+
+	List<Spot> selectByOrderByWishCountWithLimit(@Param("spotCategoryId") int spotCategoryId,
+			@Param("limit") int limit);
+
+	List<Spot> selectByOrderByFootprintCountWithLimit(@Param("spotCategoryId") int spotCategoryId,
+			@Param("limit") int limit);
+
+	List<Spot> selectByOrderByAverageReviewRating(@Param("spotCategoryId") int spotCategoryId,
+			@Param("limit") int limit);
+
 	List<Spot> selectBySpotNameKeyword(String keyword);
 }
