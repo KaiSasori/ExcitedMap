53c78f241e01baa04026f12bf1446d10171722bb
Zhou Xinan <zhouxinan@protonmail.ch>
2016-06-01 10:11:14 +0800

    Delete old spot question pojos and mappers.

--- ExcitedMap/src/main/java/com/excitedmap/dao/SpotQuestionnaireMapperImpl.java
+++
@@ -1,9 +0,0 @@
-package com.excitedmap.dao;
-
-import java.util.List;
-
-import com.excitedmap.pojo.SpotQuestionnaire;
-
-public interface SpotQuestionnaireMapperImpl extends SpotQuestionnaireMapper {
-	List<SpotQuestionnaire> selectBySpotId(int spotId);
-}
