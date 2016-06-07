a4d719347cc0d3a8d2cd75aa4d3b355936b3cf07
Zhou Xinan <zhouxinan@protonmail.ch>
2016-06-07 11:24:31 +0800

    Add session control for ReviewController.

--- ExcitedMap/src/main/java/com/excitedmap/controller/ReviewController.java
+++ ExcitedMap/src/main/java/com/excitedmap/controller/ReviewController.java
@@ -5,25 +5,31 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.excitedmap.pojo.ReviewImpl;
 import com.excitedmap.pojo.ReviewPhoto;
+import com.excitedmap.pojo.User;
 import com.excitedmap.service.ReviewService;
 
 @RestController
 @RequestMapping("/review")
+@SessionAttributes("loggedInUser")
 public class ReviewController {
 	@Resource
 	private ReviewService reviewService;
 
 	@RequestMapping(method = RequestMethod.PUT)
-	public ResponseEntity<Void> executeAddReview(@RequestBody ReviewImpl review) {
+	public ResponseEntity<Void> executeAddReview(@ModelAttribute("loggedInUser") User user,
+			@RequestBody ReviewImpl review) {
+		review.setUserId(user.getUserId());
 		reviewService.addReview(review);
 		return new ResponseEntity<Void>(HttpStatus.CREATED);
 	}
