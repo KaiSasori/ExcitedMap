c81c8a95158e94a03b072f3708ffad316d145746
VermouthMY <shqg>
2016-06-07 14:14:11 +0800

    Implement list by order for different category

--- ExcitedMap/src/main/webapp/www/js/index.js
+++ ExcitedMap/src/main/webapp/www/js/index.js
@@ -18,9 +18,9 @@ function getSpotListOrderByAverageReviewRating(){
             	var spotAddress = data[i].spotAddress;
             	var spotDescription = data[i].spotDescription;
             	var averageReviewRating = data[i].averageReviewRating;
+            	var nameString = "评分：";
                 // TODO
-                //console.log(spotName + " " + averageReviewRating);
-                addSpotListOrderByAverageReviewRating(i, spotName, averageReviewRating);
+                addSpotListOrderByNum(i, spotName, averageReviewRating, nameString);
             }
 			//console.log(data);
 		},
@@ -28,13 +28,88 @@ function getSpotListOrderByAverageReviewRating(){
 }
 
 //添加函数，将获取到的结果填入页面list中
-function addSpotListOrderByAverageReviewRating(i, spotName, averageReviewRating){
+function addSpotListOrderByNum(i, spotName, totalNum, nameString){
 	var num = i + 1;
 	var element1Id = "showSpotName" + num;
 	var element2Id = "showSpotNum" + num;
     var p1 = document.getElementById(element1Id);
     p1.innerHTML = spotName;
     var p2 = document.getElementById(element2Id);
-    p2.innerHTML = "评分：" + averageReviewRating;
+    p2.innerHTML = nameString + totalNum;
     
+}
+
+//根据收藏数获取spotlist，有limit数量限制
+function getSpotListOrderByFavoriteCount(){
+	$.ajax({
+		type : "GET",
+		url : "/spot/category/"+spotCategoryId+"/spotListOrderByFavoriteCountWithLimit?limit=3",
+		processData : false,
+		contentType : "application/json; charset=utf-8",
+		success : function(data) {// data is list<spot>
+            for(var i=0; i<data.length; i++){
+            	var spotId = data[i].spotId;
+            	var spotCategoryId = data[i].spotCategoryId;
+            	var spotName = data[i].spotName;
+            	var spotAddress = data[i].spotAddress;
+            	var spotDescription = data[i].spotDescription;
+            	var spotFavoriteCount = data[i].spotFavoriteCount;
+            	var nameString = "收藏：";
+                // TODO
+                //console.log(spotName + " " + spotFavoriteCount);
+                addSpotListOrderByNum(i, spotName, spotFavoriteCount, nameString);
+            }
+			//console.log(data);
+		},
+	});
+}
+
+//根据足迹数获取spotlist，有limit数量限制
+function getSpotListOrderByFootprintCount(){
+	$.ajax({
+		type : "GET",
+		url : "/spot/category/"+spotCategoryId+"/spotListOrderByFootprintCountWithLimit?limit=3",
+		processData : false,
+		contentType : "application/json; charset=utf-8",
+		success : function(data) {// data is list<spot>
+            for(var i=0; i<data.length; i++){
+            	var spotId = data[i].spotId;
+            	var spotCategoryId = data[i].spotCategoryId;
+            	var spotName = data[i].spotName;
+            	var spotAddress = data[i].spotAddress;
+            	var spotDescription = data[i].spotDescription;
+            	var spotFootprintCount = data[i].spotFootprintCount;
+            	var nameString = "足迹：";
+                // TODO
+                //console.log(spotName + " " + spotFavoriteCount);
+                addSpotListOrderByNum(i, spotName, spotFootprintCount, nameString);
+            }
+			//console.log(data);
+		},
+	});
+}
+
+//根据心愿数获取spotlist，有limit数量限制
+function getSpotListOrderByWishCount(){
+	$.ajax({
+		type : "GET",
+		url : "/spot/category/"+spotCategoryId+"/spotListOrderByWishCountWithLimit?limit=3",
+		processData : false,
+		contentType : "application/json; charset=utf-8",
+		success : function(data) {// data is list<spot>
+            for(var i=0; i<data.length; i++){
+            	var spotId = data[i].spotId;
+            	var spotCategoryId = data[i].spotCategoryId;
+            	var spotName = data[i].spotName;
+            	var spotAddress = data[i].spotAddress;
+            	var spotDescription = data[i].spotDescription;
+            	var spotWishCount = data[i].spotWishCount;
+            	var nameString = "心愿：";
+                // TODO
+                //console.log(spotName + " " + spotFavoriteCount);
+                addSpotListOrderByNum(i, spotName, spotWishCount, nameString);
+            }
+			//console.log(data);
+		},
+	});
 }
