596b87efb000ce321cad03d50d4c93ed135fe42b
VermouthMY <shqg>
2016-06-11 16:29:14 +0800

    Rebuild code

--- ExcitedMap/src/main/webapp/www/js/controllers.js
+++ ExcitedMap/src/main/webapp/www/js/controllers.js
@@ -2,9 +2,16 @@
  * Created by Jinkai on 2016/5/20.
  */
 var spotCategoryId = 0;
+var listMode = 0;
+//history search框功能
+var searchString = "wishCount";
+
+
+
+
 
 angular.module('myApp.controllers', [])
-.controller('TutorialCtrl', function($scope, $state, $ionicViewService) {
+.controller('TutorialCtrl', function($scope, $state, $ionicViewService, $rootScope) {
         //首次登陆
         window.localStorage['didTutorial'] = false;
 
@@ -17,16 +24,128 @@ angular.module('myApp.controllers', [])
         $scope.goMain = function() {
             startApp();
         }
+
+        $rootScope.spotCategoryId = 0;
 })
 
 .controller('HomeTabCtrl', function($scope) {
-        console.log('HomeTabCtrl');
-        // 百度地图API功能
-        var map = new BMap.Map("allmap");
-        map.centerAndZoom(new BMap.Point(121.484, 31.195), 11);
-        map.addControl(new BMap.MapTypeControl());
-        map.setCurrentCity("上海");
-        map.enableScrollWheelZoom(true);
+    console.log('HomeTabCtrl');
+    // 百度地图API功能
+    var map = new BMap.Map("allmap");
+    map.centerAndZoom(new BMap.Point(121.484, 31.195), 11);
+    map.addControl(new BMap.MapTypeControl());
+    map.setCurrentCity("上海");
+    map.enableScrollWheelZoom(true);
+
+    $scope.button1ClickTime = 0;
+    $scope.button2ClickTime = 0;
+    $scope.keys1 = [];
+    $scope.keys2 = [];
+
+    $scope.showPrintSpot = function(index){
+        if (index == 1){
+            if ($scope.button1ClickTime == 0){
+                //从数据库中提取categoryId为1的spot
+                //categoryId搜索spot
+                $scope.keys1 = [];
+                $.ajax({
+                    type : "GET",
+                    url : "/spot/category/1",
+                    processData : false,
+                    contentType : "application/json; charset=utf-8",
+                    success : function(data) {// data is list<spot>
+                        for(var i=0; i<data.length; i++){
+                            var spotName = data[i].spotName;
+                            // TODO
+                            $scope.keys1.push(spotName);
+                        }
+                        // 百度地图API功能
+                        var map = new BMap.Map("allmap");            // 创建Map实例
+                        map.centerAndZoom(new BMap.Point(121.484, 31.195), 11);
+                        var local = new BMap.LocalSearch(map, {
+                            renderOptions:{map: map},
+                            pageCapacity:1
+                        });
+                        local.searchInBounds($scope.keys1, map.getBounds());
+                        $scope.button1ClickTime = 1;
+                    },
+                }); 
+            }
+            else{
+                if ($scope.button2ClickTime == 1){
+                    // 百度地图API功能
+                    var map = new BMap.Map("allmap");            // 创建Map实例
+                    map.centerAndZoom(new BMap.Point(121.484, 31.195), 11);
+                    //var myKeys = ["中山公园", "世纪公园", "锦江乐园", "复兴公园"];
+                    var local = new BMap.LocalSearch(map, {
+                        renderOptions:{map: map},
+                        pageCapacity:1
+                    });
+                    local.searchInBounds($scope.keys2, map.getBounds());
+                    $scope.button1ClickTime = 0;
+                }
+                else{
+                    // 百度地图API功能
+                    var map = new BMap.Map("allmap");
+                    map.centerAndZoom(new BMap.Point(121.484, 31.195), 11);
+                    map.addControl(new BMap.MapTypeControl());
+                    map.setCurrentCity("上海");
+                    map.enableScrollWheelZoom(true);
+                    $scope.button1ClickTime = 0;
+                }
+            }
+        }else if (index == 2){
+            if ($scope.button2ClickTime == 0){
+                //从数据库中提取categoryId为1的spot
+                //categoryId搜索spot
+                $scope.keys2 = [];
+                $.ajax({
+                    type : "GET",
+                    url : "/spot/category/2",
+                    processData : false,
+                    contentType : "application/json; charset=utf-8",
+                    success : function(data) {// data is list<spot>
+                        for(var i=0; i<data.length; i++){
+                            var spotName = data[i].spotName;
+                            // TODO
+                            $scope.keys2.push(spotName);
+                        }
+                        // 百度地图API功能
+                        var map = new BMap.Map("allmap");            // 创建Map实例
+                        map.centerAndZoom(new BMap.Point(121.484, 31.195), 11);
+                        var local = new BMap.LocalSearch(map, {
+                            renderOptions:{map: map},
+                            pageCapacity:1
+                        });
+                        local.searchInBounds($scope.keys2, map.getBounds());
+                        $scope.button2ClickTime = 1;
+                    },
+                }); 
+            }
+            else{
+                if ($scope.button1ClickTime == 1){
+                    // 百度地图API功能
+                    var map = new BMap.Map("allmap");            // 创建Map实例
+                    map.centerAndZoom(new BMap.Point(121.484, 31.195), 11);
+                    var local = new BMap.LocalSearch(map, {
+                        renderOptions:{map: map},
+                        pageCapacity:1
+                    });
+                    local.searchInBounds($scope.keys1, map.getBounds());
+                    $scope.button2ClickTime = 0;
+                }
+                else{
+                    // 百度地图API功能
+                    var map = new BMap.Map("allmap");
+                    map.centerAndZoom(new BMap.Point(121.484, 31.195), 11);
+                    map.addControl(new BMap.MapTypeControl());
+                    map.setCurrentCity("上海");
+                    map.enableScrollWheelZoom(true);
+                    $scope.button2ClickTime = 0;
+                }
+            }
+        }
+    };
 })
 
 .controller('NearByTabCtrl', function($scope) {
@@ -38,7 +157,7 @@ angular.module('myApp.controllers', [])
         map.enableScrollWheelZoom(true);
 })
 
-.controller('AppCtrl', function ($scope, $ionicPopover,$location, $ionicPopup) {
+.controller('AppCtrl', function ($scope, $state, $ionicPopover,$location, $ionicPopup, $rootScope) {
 
         $scope.login = function(user) {
 
@@ -57,6 +176,9 @@ angular.module('myApp.controllers', [])
 
         $ionicPopover.fromTemplateUrl('partials/popover.html', function (popover) {
             $scope.popover = popover;
+
+            
+
         });
 
         $scope.showAlert = function(msg) {
@@ -65,6 +187,186 @@ angular.module('myApp.controllers', [])
                 template: msg
             });
         };
+
+
+        
+        
+
+
+        $scope.spotList = [];
+
+
+        //排序方法
+        $scope.listSpots = function(){
+            console.log("listMode = " + listMode);
+            if (listMode == 1){
+                $scope.getSpotListOrderByAverageReviewRating();
+            }
+            else if(listMode == 2){
+                $scope.getSpotListOrderByFavoriteCount();
+            }
+            else if(listMode == 3){
+                $scope.getSpotListOrderByFootprintCount();
+            }
+            else if(listMode == 4){
+                $scope.getSpotListOrderByWishCount();
+            }
+            else if(listMode == 5){
+                $scope.getSpotListOrderByPopularity();
+            }
+        };
+
+        //5种排序方法
+        //根据平均评论分数获取spotlist，有limit数量限制
+        $scope.getSpotListOrderByAverageReviewRating = function(){
+            $scope.spotList = [];
+            $.ajax({
+                type : "GET",
+                url : "/spot/category/"+$rootScope.spotCategoryId+"/spotListOrderByAverageReviewRatingWithLimit?limit=3",
+                processData : false,
+                contentType : "application/json; charset=utf-8",
+                success : function(data) {// data is list<spot>
+                    for(var i=0; i<data.length; i++){
+                        var spot = {};
+                        spot.spotId = data[i].spotId;
+                        spot.spotCategoryId = data[i].spotCategoryId;
+                        spot.spotName = data[i].spotName;
+                        spot.spotAddress = data[i].spotAddress;
+                        spot.spotDescription = data[i].spotDescription;
+                        spot.rating = data[i].averageReviewRating;
+                        spot.nameString = "评分：";
+                        // TODO
+                        $scope.spotList.push(spot);
+                        //console.log(spotName);
+                    }
+                    console.log($scope.spotList);
+                    $state.go('tabs.list.detail_list1');
+                },
+            });
+        };
+
+        //根据收藏数获取spotlist，有limit数量限制
+        $scope.getSpotListOrderByFavoriteCount = function(){
+            $scope.spotList = [];
+            $.ajax({
+                type : "GET",
+                url : "/spot/category/"+$rootScope.spotCategoryId+"/spotListOrderByFavoriteCountWithLimit?limit=3",
+                processData : false,
+                contentType : "application/json; charset=utf-8",
+                success : function(data) {// data is list<spot>
+                    for(var i=0; i<data.length; i++){
+                        var spot = {};
+                        spot.spotId = data[i].spotId;
+                        spot.spotCategoryId = data[i].spotCategoryId;
+                        spot.spotName = data[i].spotName;
+                        spot.spotAddress = data[i].spotAddress;
+                        spot.spotDescription = data[i].spotDescription;
+                        spot.rating = data[i].spotFavoriteCount;
+                        spot.nameString = "收藏：";
+                        // TODO
+                        $scope.spotList.push(spot);
+                    }
+                    console.log($scope.spotList);
+                    $state.go('tabs.list.detail_list1');
+                },
+            });
+        };
+
+
+        //根据足迹数获取spotlist，有limit数量限制
+        $scope.getSpotListOrderByFootprintCount = function(){
+            $scope.spotList = [];
+            $.ajax({
+                type : "GET",
+                url : "/spot/category/"+$rootScope.spotCategoryId+"/spotListOrderByFootprintCountWithLimit?limit=3",
+                processData : false,
+                contentType : "application/json; charset=utf-8",
+                success : function(data) {// data is list<spot>
+                    for(var i=0; i<data.length; i++){
+                        var spot = {};
+                        spot.spotId = data[i].spotId;
+                        spot.spotCategoryId = data[i].spotCategoryId;
+                        spot.spotName = data[i].spotName;
+                        spot.spotAddress = data[i].spotAddress;
+                        spot.spotDescription = data[i].spotDescription;
+                        spot.rating = data[i].spotFootprintCount;
+                        spot.nameString = "足迹：";
+                        // TODO
+                        $scope.spotList.push(spot);
+                    }
+                    console.log($scope.spotList);
+                    $state.go('tabs.list.detail_list1');
+                },
+            });
+        };
+
+        //根据心愿数获取spotlist，有limit数量限制
+        $scope.getSpotListOrderByWishCount = function(){
+            $scope.spotList = [];
+            $.ajax({
+                type : "GET",
+                url : "/spot/category/"+$rootScope.spotCategoryId+"/spotListOrderByWishCountWithLimit?limit=3",
+                processData : false,
+                contentType : "application/json; charset=utf-8",
+                success : function(data) {// data is list<spot>
+                    for(var i=0; i<data.length; i++){
+                        var spot = {};
+                        spot.spotId = data[i].spotId;
+                        spot.spotCategoryId = data[i].spotCategoryId;
+                        spot.spotName = data[i].spotName;
+                        spot.spotAddress = data[i].spotAddress;
+                        spot.spotDescription = data[i].spotDescription;
+                        spot.rating = data[i].spotWishCount;
+                        spot.nameString = "心愿：";
+                        // TODO
+                        $scope.spotList.push(spot);
+                    }
+                    console.log($scope.spotList);
+                    $state.go('tabs.list.detail_list1');
+                },
+            });
+        };
+
+        //根据popularity获取spotlist，有limit数量限制
+        $scope.getSpotListOrderByPopularity = function(){
+            $scope.spotList = [];
+            $.ajax({
+                type : "GET",
+                url : "/spot/category/"+$rootScope.spotCategoryId+"/spotListOrderByPopularityWithLimit?limit=3",
+                processData : false,
+                contentType : "application/json; charset=utf-8",
+                success : function(data) {// data is list<spot>
+                    for(var i=0; i<data.length; i++){
+                        var spot = {};
+                        spot.spotId = data[i].spotId;
+                        spot.spotCategoryId = data[i].spotCategoryId;
+                        spot.spotName = data[i].spotName;
+                        spot.spotAddress = data[i].spotAddress;
+                        spot.spotDescription = data[i].spotDescription;
+                        spot.rating = data[i].spotPopularity;
+                        spot.nameString = "热门度：";
+                        // TODO
+                        $scope.spotList.push(spot);
+                    }
+                    console.log($scope.spotList);
+                    $state.go('tabs.list.detail_list1');
+                },
+            });
+        };
+
+        $scope.goSpot = function(spot){
+            console.log("yyyyyy");
+            console.log(spot.spotId);
+        };
+
+
+
+
+
+
+
+
+
 })
 
 .controller('DetailCtrl', function($scope) {
@@ -77,11 +379,13 @@ angular.module('myApp.controllers', [])
         map.enableScrollWheelZoom(true);
 })
 
-.controller('ListCtrl', function($scope){
+.controller('ListCtrl', function($scope, $rootScope){
         $scope.onTabSelected = function(index){
-            spotCategoryId = index;
+            $rootScope.spotCategoryId = index;
+            //spotCategoryId = index;
             console.log("success! : " + spotCategoryId);
         };
+
 })
 
 .controller('ManagementCtrl', function($scope) {
@@ -92,6 +396,8 @@ angular.module('myApp.controllers', [])
         map.addControl(new BMap.MapTypeControl());
         map.setCurrentCity("上海");
         map.enableScrollWheelZoom(true);
+
+
 })
 
 .controller('HistoryCtrl', function($scope) {
@@ -102,6 +408,7 @@ angular.module('myApp.controllers', [])
             }
         });
 })
+<<<<<<< Updated upstream
 
 .controller('CommandCtrl', function($scope) {
 
@@ -121,3 +428,5 @@ angular.module('myApp.controllers', [])
         map.addControl(new BMap.MapTypeControl());
         map.setCurrentCity("上海");
 })
+=======
+>>>>>>> Stashed changes
--- ExcitedMap/src/main/webapp/www/js/index.js
+++ ExcitedMap/src/main/webapp/www/js/index.js
@@ -1,229 +1,10 @@
 //全局变量
 console.log("this:" + spotCategoryId);
 
-
-//spotController
-//根据平均评论分数获取spotlist，有limit数量限制
-function getSpotListOrderByAverageReviewRating(){
-	$.ajax({
-		type : "GET",
-		url : "/spot/category/"+spotCategoryId+"/spotListOrderByAverageReviewRatingWithLimit?limit=3",
-		processData : false,
-		contentType : "application/json; charset=utf-8",
-		success : function(data) {// data is list<spot>
-            for(var i=0; i<data.length; i++){
-            	var spotId = data[i].spotId;
-            	var spotCategoryId = data[i].spotCategoryId;
-            	var spotName = data[i].spotName;
-            	var spotAddress = data[i].spotAddress;
-            	var spotDescription = data[i].spotDescription;
-            	var averageReviewRating = data[i].averageReviewRating;
-            	var nameString = "评分：";
-                // TODO
-                addSpotListOrderByNum(i, spotName, averageReviewRating, nameString);
-            }
-			//console.log(data);
-		},
-	});
-}
-
-//添加函数，将获取到的结果填入页面list中
-function addSpotListOrderByNum(i, spotName, totalNum, nameString){
-	var num = i + 1;
-	var element1Id = "showSpotName" + num;
-	var element2Id = "showSpotNum" + num;
-    var p1 = document.getElementById(element1Id);
-    p1.innerHTML = spotName;
-    var p2 = document.getElementById(element2Id);
-    p2.innerHTML = nameString + totalNum;
-    
-}
-
-//根据收藏数获取spotlist，有limit数量限制
-function getSpotListOrderByFavoriteCount(){
-	$.ajax({
-		type : "GET",
-		url : "/spot/category/"+spotCategoryId+"/spotListOrderByFavoriteCountWithLimit?limit=3",
-		processData : false,
-		contentType : "application/json; charset=utf-8",
-		success : function(data) {// data is list<spot>
-            for(var i=0; i<data.length; i++){
-            	var spotId = data[i].spotId;
-            	var spotCategoryId = data[i].spotCategoryId;
-            	var spotName = data[i].spotName;
-            	var spotAddress = data[i].spotAddress;
-            	var spotDescription = data[i].spotDescription;
-            	var spotFavoriteCount = data[i].spotFavoriteCount;
-            	var nameString = "收藏：";
-                // TODO
-                //console.log(spotName + " " + spotFavoriteCount);
-                addSpotListOrderByNum(i, spotName, spotFavoriteCount, nameString);
-            }
-			//console.log(data);
-		},
-	});
-}
-
-//根据足迹数获取spotlist，有limit数量限制
-function getSpotListOrderByFootprintCount(){
-	$.ajax({
-		type : "GET",
-		url : "/spot/category/"+spotCategoryId+"/spotListOrderByFootprintCountWithLimit?limit=3",
-		processData : false,
-		contentType : "application/json; charset=utf-8",
-		success : function(data) {// data is list<spot>
-            for(var i=0; i<data.length; i++){
-            	var spotId = data[i].spotId;
-            	var spotCategoryId = data[i].spotCategoryId;
-            	var spotName = data[i].spotName;
-            	var spotAddress = data[i].spotAddress;
-            	var spotDescription = data[i].spotDescription;
-            	var spotFootprintCount = data[i].spotFootprintCount;
-            	var nameString = "足迹：";
-                // TODO
-                //console.log(spotName + " " + spotFavoriteCount);
-                addSpotListOrderByNum(i, spotName, spotFootprintCount, nameString);
-            }
-			//console.log(data);
-		},
-	});
-}
-
-//根据心愿数获取spotlist，有limit数量限制
-function getSpotListOrderByWishCount(){
-	$.ajax({
-		type : "GET",
-		url : "/spot/category/"+spotCategoryId+"/spotListOrderByWishCountWithLimit?limit=3",
-		processData : false,
-		contentType : "application/json; charset=utf-8",
-		success : function(data) {// data is list<spot>
-            for(var i=0; i<data.length; i++){
-            	var spotId = data[i].spotId;
-            	var spotCategoryId = data[i].spotCategoryId;
-            	var spotName = data[i].spotName;
-            	var spotAddress = data[i].spotAddress;
-            	var spotDescription = data[i].spotDescription;
-            	var spotWishCount = data[i].spotWishCount;
-            	var nameString = "心愿：";
-                // TODO
-                //console.log(spotName + " " + spotFavoriteCount);
-                addSpotListOrderByNum(i, spotName, spotWishCount, nameString);
-            }
-			//console.log(data);
-		},
-	});
-}
-
-
-//图层标志显示js
-var button1ClickTime = 0;
-var button2ClickTime = 0;
-var keys1 = [];
-var keys2 = [];
-function showPrintSpot(index){
-	if (index == 1){
-		if (button1ClickTime == 0){
-			//从数据库中提取categoryId为1的spot
-			//categoryId搜索spot
-			keys1 = [];
-			$.ajax({
-				type : "GET",
-				url : "/spot/category/1",
-				processData : false,
-				contentType : "application/json; charset=utf-8",
-				success : function(data) {// data is list<spot>
-            		for(var i=0; i<data.length; i++){
-            			var spotName = data[i].spotName;
-                		// TODO
-               			keys1.push(spotName);
-               		}
-					// 百度地图API功能
-					var map = new BMap.Map("allmap");            // 创建Map实例
-					map.centerAndZoom(new BMap.Point(121.484, 31.195), 11);
-					var local = new BMap.LocalSearch(map, {
-						renderOptions:{map: map},
-						pageCapacity:1
-					});
-					local.searchInBounds(keys1, map.getBounds());
-					button1ClickTime = 1;
-				},
-			});	
-		}
-		else{
-			if (button2ClickTime == 1){
-				// 百度地图API功能
-				var map = new BMap.Map("allmap");            // 创建Map实例
-				map.centerAndZoom(new BMap.Point(121.484, 31.195), 11);
-				//var myKeys = ["中山公园", "世纪公园", "锦江乐园", "复兴公园"];
-				var local = new BMap.LocalSearch(map, {
-					renderOptions:{map: map},
-					pageCapacity:1
-				});
-				local.searchInBounds(keys2, map.getBounds());
-				button1ClickTime = 0;
-			}
-			else{
-				// 百度地图API功能
-				var map = new BMap.Map("allmap");
-        		map.centerAndZoom(new BMap.Point(121.484, 31.195), 11);
-        		map.addControl(new BMap.MapTypeControl());
-        		map.setCurrentCity("上海");
-        		map.enableScrollWheelZoom(true);
-				button1ClickTime = 0;
-			}
-		}
-	}else if (index == 2){
-		if (button2ClickTime == 0){
-			//从数据库中提取categoryId为1的spot
-			//categoryId搜索spot
-			keys2 = [];
-			$.ajax({
-				type : "GET",
-				url : "/spot/category/2",
-				processData : false,
-				contentType : "application/json; charset=utf-8",
-				success : function(data) {// data is list<spot>
-            		for(var i=0; i<data.length; i++){
-            			var spotName = data[i].spotName;
-                		// TODO
-               			keys2.push(spotName);
-               		}
-					// 百度地图API功能
-					var map = new BMap.Map("allmap");            // 创建Map实例
-					map.centerAndZoom(new BMap.Point(121.484, 31.195), 11);
-					var local = new BMap.LocalSearch(map, {
-						renderOptions:{map: map},
-						pageCapacity:1
-					});
-					local.searchInBounds(keys2, map.getBounds());
-					button2ClickTime = 1;
-				},
-			});	
-		}
-		else{
-			if (button1ClickTime == 1){
-				// 百度地图API功能
-				var map = new BMap.Map("allmap");            // 创建Map实例
-				map.centerAndZoom(new BMap.Point(121.484, 31.195), 11);
-				var local = new BMap.LocalSearch(map, {
-					renderOptions:{map: map},
-					pageCapacity:1
-				});
-				local.searchInBounds(keys1, map.getBounds());
-				button2ClickTime = 0;
-			}
-			else{
-				// 百度地图API功能
-				var map = new BMap.Map("allmap");
-        		map.centerAndZoom(new BMap.Point(121.484, 31.195), 11);
-        		map.addControl(new BMap.MapTypeControl());
-        		map.setCurrentCity("上海");
-        		map.enableScrollWheelZoom(true);
-				button2ClickTime = 0;
-			}
-		}
-	}
-}
+function setListMode(index){
+	listMode = index;
+	console.log("listMode = " + listMode);
+} 
 
 //线路规划，目前会有不稳定错误，是百度api问题，不知如何解决
 function showManagementBetweenSpots(){
@@ -235,6 +16,8 @@ function showManagementBetweenSpots(){
 	var driving = new BMap.DrivingRoute(map, {renderOptions:{map: map, autoViewport: true},policy: BMAP_DRIVING_POLICY_LEAST_TIME});
 	driving.search(start,end);
 }
+
+
 
 //search框功能
 var searchString = "wishCount";
@@ -251,6 +34,7 @@ function searchStringNum(index){
 	else if(index == 4)
 		searchString = "wishCount";
 }
+
 function clearSearch(){
 	for (var num = 1; num <=3; num++){
 		var element1Id = "searchSpotName" + num;
